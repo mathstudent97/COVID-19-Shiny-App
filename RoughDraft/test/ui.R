79b305df2b6e55df6043e96efae2da0dfe5bbe3c
Verna Marie Maullon <59632000+mathstudent97@users.noreply.github.com>
2020-09-25 02:12:32 -0400

    Add files via upload

---
+++ RoughDraft/test/ui.R
@@ -0,0 +1,33 @@
+body_about <- dashboardBody(
+    fluidRow(
+        fluidRow(
+            column(
+                box(
+                    title = div("About", style = "padding-right: 10px", class = "h3"),
+                    column("This dashboard demonstrates some recent news about the Coronavirus pandemic. 
+            This App is a simulator, that reads from the John Hopkins dataset, and shows some data related to mortality,  
+            recovery, infected, and etc..",
+                           tags$br(),
+                           h3("COVID-19 Social Distancing"),
+                           "Please stay safe and respect social distancing, which can be tough on people and could disrupt the social and economic loop of life.C",
+                           tags$br(),
+                           width = 12
+                    ),
+                    width = 6,
+                ),
+                width = 12,
+                style = "padding: 15px"
+            )
+        )
+    )
+)
+
+
+page_about <- dashboardPage(
+    title = "About",
+    header = dashboardHeader(disable = TRUE),
+    sidebar = dashboardSidebar(disable = TRUE),
+    body = body_about
+)
+
+
