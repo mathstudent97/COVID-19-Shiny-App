1142822f0c950b397213b84548d871d2547d49f3
Verna Marie Maullon <59632000+mathstudent97@users.noreply.github.com>
2021-03-09 22:56:14 -0500

    Add files via upload

---
+++ GOODCOPY/StatisticalAnalysisWebApp/ui.R
@@ -0,0 +1,12 @@
+# User Interface
+
+ui <- fluidPage(
+  title = "COVID-19 Cases Worldwide",
+  tags$head(
+    tags$link(rel = "shortcut icon", type = "image/png", href = "logo.png")
+  ),
+  tags$style(type = "text/css", ".container-fluid {padding-left: 0px; padding-right: 0px !important;}"),
+  tags$style(type = "text/css", ".navbar {margin-bottom: 0px;}"),
+  tags$style(type = "text/css", ".content {padding: 0px;}"),
+  
+)
