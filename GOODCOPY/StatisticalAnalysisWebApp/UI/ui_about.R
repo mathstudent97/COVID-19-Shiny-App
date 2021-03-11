b93e5e208cd4291615ee05bb2a876578fea170b5
Verna Marie Maullon <59632000+mathstudent97@users.noreply.github.com>
2021-03-10 23:26:29 -0500

    Add files via upload

---
+++ GOODCOPY/StatisticalAnalysisWebApp/UI/ui_about.R
@@ -0,0 +1,41 @@
+
+
+
+body_about <- dashboardBody(
+  fluidRow(
+    fluidRow(
+      column(
+        box(
+          title = div("About this web app", style = "padding-left: 20px", class = "h2"),
+          column(
+            h3("COVID-19 Statistical Analysis Dashboard"),
+            "This dashboard displays recent stats about the COVID-19 Pandemic.
+            This web app reads public data retrieved from the John Hopkins website, and shows data related to positive cases,
+            mortality, recoveries, etc...",
+            tags$br(),
+            h3("COVID-19 & Social Distancing"),
+            "Please stay safe, keeping in mind to keep your distance towards others.",
+            tags$br(),
+            h3("Disclaimer"),
+            "This app has been created for educational purposes only.",
+            width = 12
+          ),
+          width = 6,
+        ),
+        width = 12,
+        style = "padding: 15 px"
+      )
+    )
+  )
+)
+
+
+# About page should be part of the dashboard page.
+page_about <- dashboardPage(
+  title = "About",
+  header = dashboardHeader(disable = TRUE),
+  sidebar = dashboardSidebar(disable = TRUE),
+  body = body_about
+)
+
+
---
+++ GOODCOPY/StatisticalAnalysisWebApp/UI/ui_fullTable.R
@@ -0,0 +1,5 @@
+body_fullTable <- dashboardBody(
+  tags$head(
+    tags$style(type = "text/css", "@media (min-width: 768px)")
+  )
+)
