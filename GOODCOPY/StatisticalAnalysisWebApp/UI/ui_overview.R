4158e72fc7bc27055afd642f49605ddbb8378cd0
Verna Marie Maullon <59632000+mathstudent97@users.noreply.github.com>
2021-03-22 23:44:15 -0400

    Update ui_overview.R

--- GOODCOPY/StatisticalAnalysisWebApp/UI/ui_overview.R
+++ GOODCOPY/StatisticalAnalysisWebApp/UI/ui_overview.R
@@ -1,10 +1,10 @@
-
+# Overview tab.
 
 
 body_overview <- dashboardBody(
   tags$head(
-    tags$style(type = "text/css", "#overview_map {height: 48vh !important;}"),
-    tags$style(type = "text/css", ".slider-animate-button { font-size: 20pt !important; }"),
+    tags$style(type = "text/css", "#overview_map {height: 48vh !important;}"), # Displays a map.
+    tags$style(type = "text/css", ".slider-animate-button { font-size: 20pt !important; }"), # Animation button.
     tags$style(type = "text/css", ".slider-animate-container { text-align: left !important; }"),
     tags$style(type = "text/css", "@media (max-width: 991px) { .details {display: flex; flex-direction: column; } }"),
     tags$style(type = "text/css", "@media (max-width: 991px) { .details .map { order: 1; width: 100%; } }"),
@@ -13,7 +13,7 @@ body_overview <- dashboardBody(
   ),
   fluidRow(
     fluidRow(
-      uiOutput("box_keyFigures")
+      uiOutput("box_keyFigures") # Imp. for later on.
     ),
     fluidRow(
       class = "details",
@@ -35,11 +35,11 @@ body_overview <- dashboardBody(
       column(
         sliderInput(
           "timeSlider",
-          label = "Select date",
+          label = "Select date", # So user selects their own date.
           min = min(data_evolution$date),
           max = max(data_evolution$date),
-          value = max(data_evolution$date),
-          width = "100%",
+          value = max(data_evolution$date), # This is the current value; which will obv. be the max.
+          width = "100%", # Fill the page.
           timeFormat = "%d.%m.%Y",
           animate = animationOptions(loop = TRUE)
         ),
@@ -59,5 +59,4 @@ page_overview <- dashboardPage(
 )
   
   
-
 
